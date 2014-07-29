a29f4380efdae14f4a942e7947e2093e873c7118
Jouko Strömmer <jouko.strommer@helsinki.fi>
2014-07-29 18:05:58 +0300

    Modified NiTETracker to be more robust with sensor USB disconnects, added test programs.

---
+++ examples/NiTE2-project/Screenful-NiTE2/src/screenful/apps/GestureServer.java
@@ -0,0 +1,63 @@
+package screenful.apps;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openni.DeviceInfo;
+import org.openni.OpenNI;
+import screenful.basic.NiTETracker;
+import screenful.detectors.DirectionDetector;
+import screenful.gestures.Displacement;
+import screenful.gestures.Gesture;
+import screenful.gestures.GestureListener;
+
+/**
+ * Test program for gesture recognition recovery.
+ *
+ */
+public class GestureServer {
+
+    /**
+     * Simple listener to demonstrate the directions are still being recognized.
+     */
+    static class PrintDirection implements GestureListener {
+
+        @Override
+        public void onGesture(Displacement gesture) {
+            System.out.println("" + gesture.getDirection());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        NiTETracker tracker = new NiTETracker();
+        List<DeviceInfo> devs;
+        int lastnum = 0;
+        // create a gesture listener
+        //ScreenfulSwipes swipes = new ScreenfulSwipes(tracker, new Gesture(new DirectionDetector(5), 5, 50));
+        Gesture swipe = new Gesture(new DirectionDetector(5), 5, 10);
+        tracker.addHandsListener(swipe);
+        PrintDirection printDir = new PrintDirection();
+        swipe.addListener(printDir);
+
+        while (true) {
+
+            devs = OpenNI.enumerateDevices();
+            if (lastnum != devs.size()) {
+                lastnum = devs.size();
+                System.out.println("Devices connected: " + lastnum);
+            }
+            // if no devices, wait until a device is available again
+            while (devs.size() < 1) {
+                Thread.yield();
+                devs = OpenNI.enumerateDevices();
+            }
+            // when device is connected, do something every now and then
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(GestureServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
--- examples/NiTE2-project/Screenful-NiTE2/src/screenful/basic/NiTETracker.java
+++ examples/NiTE2-project/Screenful-NiTE2/src/screenful/basic/NiTETracker.java
@@ -12,6 +12,9 @@ import com.primesense.nite.UserTrackerFrameRef;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openni.DeviceInfo;
 import org.openni.OpenNI;
 
 /**
@@ -25,19 +28,28 @@ import org.openni.OpenNI;
  * You can pass the NiTETracker object to a Visualization object to get a
  * graphical window, see BonesAndHandsViewer.
  *
+ * To recover from intermittent disconnects of the sensor, the object attempts
+ * to keep track when a sensor disconnects and stops updating frames until a
+ * device is available again.
+ *
  */
-public class NiTETracker implements HandTracker.NewFrameListener, UserTracker.NewFrameListener {
+public class NiTETracker implements
+        HandTracker.NewFrameListener,
+        UserTracker.NewFrameListener,
+        OpenNI.DeviceDisconnectedListener,
+        OpenNI.DeviceConnectedListener {
 
-    private final ArrayList<HandsListener> handsListeners;
-    private final ArrayList<BonesListener> bonesListeners;
+    private ArrayList<HandsListener> handsListeners;
+    private ArrayList<BonesListener> bonesListeners;
 
-    final HandTracker handTracker;
-    final UserTracker userTracker;
+    HandTracker handTracker;
+    UserTracker userTracker;
 
     HandTrackerFrameRef lastHandFrame;
     UserTrackerFrameRef lastUserFrame;
 
     BufferedImage bufferedImage;
+    boolean deviceConnected;
 
     /**
      * Return hand tracker for reading hand positions and gestures
@@ -121,6 +133,14 @@ public class NiTETracker implements HandTracker.NewFrameListener, UserTracker.Ne
     }
 
     /**
+     * Remove all hands and bones listeners
+     */
+    public synchronized void removeAllListeners() {
+        handsListeners.clear();
+        bonesListeners.clear();
+    }
+
+    /**
      * Notify listeners of new hand frames
      */
     private void notifyHandsListeners() {
@@ -139,24 +159,74 @@ public class NiTETracker implements HandTracker.NewFrameListener, UserTracker.Ne
     }
 
     /**
+     * Wait until a device is connected. Check every second.
+     */
+    private void waitForDevice() {
+        while (OpenNI.enumerateDevices().size() < 1) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(NiTETracker.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("No devices found, waiting...");
+        }
+        deviceConnected = true;
+    }
+
+    private void initialize() {
+        handsListeners = new ArrayList<>();
+        bonesListeners = new ArrayList<>();
+
+        OpenNI.initialize();
+        System.out.println("OPENNI INIT");
+        waitForDevice();
+        NiTE.initialize();
+        System.out.println("NITE INIT");
+        createTrackers();
+    }
+
+    /**
+     * Shut down NiTE and OpenNI.
+     */
+    private void shutdown() {
+        NiTE.shutdown();
+        OpenNI.shutdown();
+    }
+
+    /**
+     * Create hand and user trackers.
+     */
+    private void createTrackers() {
+        try {
+            userTracker = UserTracker.create();
+            userTracker.addNewFrameListener(this);
+        } catch (RuntimeException e) {
+            System.out.println("CREATING USER TRACKER FAILED");
+        }
+
+        try {
+            handTracker = HandTracker.create();
+            handTracker.addNewFrameListener(this);
+
+            handTracker.startGestureDetection(GestureType.CLICK);
+            handTracker.startGestureDetection(GestureType.WAVE);
+        } catch (RuntimeException e) {
+            System.out.println("CREATING HAND TRACKER FAILED");
+        }
+
+    }
+
+    /**
      * Initialize OpenNI and NiTE, create user and hand trackers, add NuiTracker
      * to their listeners and configure hand tracker to look for click and wave
      * gestures to initiate hand tracking.
      */
     public NiTETracker() {
-        OpenNI.initialize();
-        NiTE.initialize();
-        handsListeners = new ArrayList<>();
-        bonesListeners = new ArrayList<>();
+        deviceConnected = false;
+        initialize();
 
-        userTracker = UserTracker.create();
-        userTracker.addNewFrameListener(this);
-
-        handTracker = HandTracker.create();
-        handTracker.addNewFrameListener(this);
-
-        handTracker.startGestureDetection(GestureType.CLICK);
-        handTracker.startGestureDetection(GestureType.WAVE);
+        OpenNI.addDeviceDisconnectedListener(this);
+        OpenNI.addDeviceConnectedListener(this);
     }
 
     /**
@@ -187,44 +257,61 @@ public class NiTETracker implements HandTracker.NewFrameListener, UserTracker.Ne
 
     @Override
     public void onNewFrame(HandTracker ht) {
-        if (lastHandFrame != null) {
-            lastHandFrame.release();
-            lastHandFrame = null;
-        }
-
-        lastHandFrame = ht.readFrame();
-
-        if (lastHandFrame.getGestures().size() > 0) {
-            System.out.println("Gestures: " + lastHandFrame.getGestures().size());
-        }
-        // check if any gesture detected
-        for (GestureData gesture : lastHandFrame.getGestures()) {
-            if (gesture.isComplete()) {
-                // start hand tracking
-                System.out.println("Starting hand tracking");
-                handTracker.startHandTracking(gesture.getCurrentPosition());
+        if (deviceConnected) {
+            if (lastHandFrame != null) {
+                lastHandFrame.release();
+                lastHandFrame = null;
             }
+
+            lastHandFrame = ht.readFrame();
+
+            if (lastHandFrame.getGestures().size() > 0) {
+                System.out.println("Gestures: " + lastHandFrame.getGestures().size());
+            }
+            // check if any gesture detected
+            for (GestureData gesture : lastHandFrame.getGestures()) {
+                if (gesture.isComplete()) {
+                    // start hand tracking
+                    System.out.println("Starting hand tracking");
+                    handTracker.startHandTracking(gesture.getCurrentPosition());
+                }
+            }
+            notifyHandsListeners();
         }
-        notifyHandsListeners();
     }
 
     @Override
     public void onNewFrame(UserTracker ut) {
-        if (lastUserFrame != null) {
-            lastUserFrame.release();
-            lastUserFrame = null;
-        }
-
-        lastUserFrame = ut.readFrame();
-
-        // check if any new user detected
-        for (UserData user : lastUserFrame.getUsers()) {
-            if (user.isNew()) {
-                // start skeleton tracking
-                System.out.println("User " + user.getId() + " found, starting tracking");
-                userTracker.startSkeletonTracking(user.getId());
+        if (deviceConnected) {
+            if (lastUserFrame != null) {
+                lastUserFrame.release();
+                lastUserFrame = null;
             }
+
+            lastUserFrame = ut.readFrame();
+
+            // check if any new user detected
+            for (UserData user : lastUserFrame.getUsers()) {
+                if (user.isNew()) {
+                    // start skeleton tracking
+                    System.out.println("User " + user.getId() + " found, starting tracking");
+                    userTracker.startSkeletonTracking(user.getId());
+                }
+            }
+            notifyBonesListeners();
         }
-        notifyBonesListeners();
+    }
+
+    @Override
+    public void onDeviceDisconnected(DeviceInfo di) {
+        System.out.println("" + di.getName() + " -- Disconnected");
+        deviceConnected = false;
+    }
+
+    @Override
+    public void onDeviceConnected(DeviceInfo di) {
+        System.out.println("" + di.getName() + " -- Connected");
+        createTrackers();
+        waitForDevice();
     }
 }
